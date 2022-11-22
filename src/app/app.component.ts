36f09882ec9e585de00fa3d501911b4d319c5341
Malaka Rakesh Kumar Reddy <83658707+rakeshvirat@users.noreply.github.com>
2022-11-22 17:50:24 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,4 +8,17 @@ import { Router } from '@angular/router';
 })
 export class AppComponent{
   title = 'assignment2';
+
+  toggleB(e:any)
+  {
+    e.target.classList.add('active')
+    if(e.target.getAttribute('id')=="tab1")
+    {
+      document.getElementById("tab2")?.classList.remove('active')
+    }
+    else if(e.target.getAttribute('id')=="tab2")
+    {
+      document.getElementById("tab1")?.classList.remove('active')
+    }
+  }
 }
